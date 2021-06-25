b2813cba4334898a7ba6f1b94e9df52907511ec0
Seeun <duck_sim@naver.com>
2021-06-25 14:37:03 +0900

    해쉬태그 구현

--- myvenv/Scripts/activate.bat
+++ myvenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\duck_\Desktop\crudd\myvenv
+set VIRTUAL_ENV=C:\Users\duck_\Desktop\crud\myvenv
 
 if not defined PROMPT set PROMPT=$P$G
 
